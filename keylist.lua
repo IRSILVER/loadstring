bb85d462b3f495372f81c753438f9406ad0683f3
VortexScripting <77008143+IRSILVER@users.noreply.github.com>
2023-09-20 10:11:49 +0200

    Create keylist.lua

---
+++ keylist.lua
@@ -0,0 +1,15 @@
+
+local keylist = {}
+
+-- Define the daily key
+keylist.daily = "test"
+
+-- Define the premium keys
+keylist.premium = {
+    "key1",
+    "key2",
+    "key3",
+    -- Add more premium keys as needed
+}
+
+return keylist
